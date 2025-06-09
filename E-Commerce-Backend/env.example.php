199bb4809d615d845213c22335edb0f47dca69ab
Kauan Ramos Fontana <kfontana@personalcard.com.br>
2025-06-09 11:37:34 -0300

    add .env.example.php

---
+++ E-Commerce-Backend/env.example.php
@@ -0,0 +1,13 @@
+<?php
+
+define("DISPLAY_ERRORS_DETAILS", true);
+
+define("DEVUTOPIA_SQLSERVER_HOST", "");
+define("DEVUTOPIA_SQLSERVER_DBNAME", "");
+define("DEVUTOPIA_SQLSERVER_USER", "");
+define("DEVUTOPIA_SQLSERVER_PASSWORD", "");
+
+define("JWT_SECRET_KEY", "");
+
+define("LOCATION_PUBLIC_API", "https://servicodados.ibge.gov.br/api/v1/localidades");
+define("CEP_PUBLIC_API", "https://api.brasilaberto.com/v1/zipcode");
